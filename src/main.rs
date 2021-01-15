df780d4dbf0cce36143c7298e3939a4f8136092f
aarrascue <aarrascue@neuroloop.de>
2021-01-15 19:00:47 +0100

    Adding content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+
+use sse::sse_controller;
+use crate::sse_controller::set_controller;
+use tokio::time::{delay_for, Duration};
+use rand::{self, Rng};
+
+pub static MESSAGE_SENDING_TIMEOUT_SECONDS: u32 = 5;
+
+#[tokio::main]
+async fn main()
+{
+	let mut rng = rand::thread_rng();
+    let controller = set_controller();          
+    loop {
+    	let ran_value = rng.gen_range(0..101);
+    	let _charger_state = controller.send(ran_value.to_string());
+    	delay_for(Duration::from_secs(MESSAGE_SENDING_TIMEOUT_SECONDS.into())).await;
+    }
 }
